9d5f1d75097a9e30d389b557f5d4802ae3f04a9f
camilamlima <camilam.oliveiralima@gmail.com>
2019-01-15 00:43:31 +0000

    first commit

---
+++ precache-manifest.3338cdb82d548c6607d048c84fa0e605.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab662b1400ff12eec96a345e3a35ba7d",
+    "url": "/static/media/rosas2.ab662b14.jpg"
+  },
+  {
+    "revision": "7ac4b190a52cc4ee31e2c88bdfee4c8e",
+    "url": "/static/media/perfil.7ac4b190.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "394a1a6bf9d50931c9ee",
+    "url": "/static/js/main.394a1a6b.chunk.js"
+  },
+  {
+    "revision": "82cd9ce32aa7fc743d84",
+    "url": "/static/js/1.82cd9ce3.chunk.js"
+  },
+  {
+    "revision": "394a1a6bf9d50931c9ee",
+    "url": "/static/css/main.6fc93372.chunk.css"
+  },
+  {
+    "revision": "8edc8b0ef90f452e71d60196913d5dce",
+    "url": "/index.html"
+  }
+];
